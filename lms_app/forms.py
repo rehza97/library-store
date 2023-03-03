54471b282c6a4931d1b879609e0f0a448c8304c9
Tebib Fethallah <fethotebib3@gmail.com>
2023-03-03 19:38:31 +0100

    make the add book works

--- lms_app/forms.py
+++ lms_app/forms.py
@@ -7,6 +7,7 @@ class BookForm(forms.ModelForm):
         fields =  [
             'title',
             'auther',
+            'photo_book',
             'photo_auther',
             'pages',
             'price',
@@ -16,8 +17,45 @@ class BookForm(forms.ModelForm):
             'category',
             ]
         widgets = {
-            'title' : forms.TextInput(attrs={
+            'title' : forms.TextInput(
+                attrs={
                 'class' : 'form-control'
-            })
+            }),
+            'auther' : forms.TextInput(
+                attrs={
+                'class' : 'form-control'
+            }),
+            'photo_auther' : forms.FileInput(
+                attrs={
+                'class' : 'form-control'
+            }),
+            'photo_book' : forms.FileInput(
+                attrs={
+                'class' : 'form-control'
+            }),
+            'pages' : forms.NumberInput(
+                attrs={
+                'class' : 'form-control'
+            }),
+            'price' : forms.NumberInput(
+                attrs={
+                'class' : 'form-control'
+            }),
+            'retal_price_day' : forms.NumberInput(
+                attrs={
+                'class' : 'form-control'
+            }),
+            'retal_period' : forms.NumberInput (
+                attrs={
+                'class' : 'form-control'
+            }),
+            'status' : forms.Select(
+                attrs={
+                'class' : 'form-control'
+            }),
+            'category' : forms.Select(
+                attrs={
+                'class' : 'form-control'
+            }),
         }
     
--- lms_app/views.py
+++ lms_app/views.py
@@ -3,6 +3,12 @@ from .models import *
 from .forms import BookForm
 # Create your views here.
 def index(request):
+    
+    if request.method == 'POST':
+        add_book = BookForm(request.POST , request.FILES)
+
+        if add_book.is_valid():
+            add_book.save()
     context = {
         'books' : Book.objects.all(),
         'category' : Category.objects.all(),
