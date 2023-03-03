553fa837413995915e29abbcfda28aa0c6b213b4
Tebib Fethallah <fethotebib3@gmail.com>
2023-03-04 00:16:49 +0100

    it is working

--- lms_app/forms.py
+++ lms_app/forms.py
@@ -1,20 +1,19 @@
 from django import forms
 from .models import Book, Category
+
+class CategoryForm(forms.ModelForm):
+    class Meta:
+        model = Category
+        fields = ['name']
+        widgets = {'name' : forms.TextInput(attrs={'class' : 'form-control'}),
+        }
+
 class BookForm(forms.ModelForm):
     class Meta:
         model = Book
         # fields =  '__all__'
         fields =  [
-            'title',
-            'auther',
-            'photo_book',
-            'photo_auther',
-            'pages',
-            'price',
-            'retal_price_day',
-            'retal_period',
-            'status',
-            'category',
+            'title','auther','photo_book','photo_auther','pages','price','retal_price_day','retal_period','status','category',
             ]
         widgets = {
             'title' : forms.TextInput(
--- lms_app/views.py
+++ lms_app/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render , get_object_or_404
+from django.shortcuts import redirect
+
 from .models import *
-from .forms import BookForm
+from .forms import BookForm , CategoryForm
 # Create your views here.
 def index(request):
     
@@ -9,10 +11,16 @@ def index(request):
 
         if add_book.is_valid():
             add_book.save()
+            
+        
+        add_cat = CategoryForm(request.POST)
+        if add_cat.is_valid():
+            add_cat.save()
     context = {
         'books' : Book.objects.all(),
         'category' : Category.objects.all(),
         'form' : BookForm(),
+        'formcat' : CategoryForm()
     }
     return render(request,'pages/index.html',context)
 
@@ -23,9 +31,25 @@ def books(request):
     }
     return render(request,'pages/books.html',context)
 
-def delete(request):
+def delete(request , id):
+    book_delete =get_object_or_404(Book , id=id)
+    if request.method == 'POST':
+        book_delete.delete()
+        return redirect('/')
+
     return render(request,'pages/delete.html')
 
-def update(request):
-    return render(request,'pages/update.html')
+def update(request,id):
+    book_id =Book.objects.get(id=id)
+    if request.method == 'POST':
+        book_save = BookForm(request.POST , request.FILES , instance=book_id)
+        if book_save.is_valid():
+            book_save.save() 
+            return redirect('/')
+    else:
+        book_save = BookForm(instance=book_id)  
+    context = {'form' : book_save}
+    
+    return render(request , 'pages/update.html',context)
+    
 
